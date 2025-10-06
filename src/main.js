0c9966a494a7ce2668586219a35fe7fce823b1f7
shiloox <ada44lovelace@gmail.com>
2025-10-06 16:59:58 -0500

    chore: configure primevue and register global ui components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
+import {Primevue} from "primevue/config";
+import Material from "@primeuix/themes/material";
+import i18n from "./i18n.js";
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css'
+import router from "./router.js";
+import pinia from "pinia";
+import {
+    Button,
+    Card,
+    ConfirmationService,
+    DialogService,
+    FloatLabel,
+    InputText,
+    Password, Toast,
+    ToastService
+} from "primevue";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(Primevue,{theme:{preset:Material}, ripple:true})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .component('pv-input-text', InputText)
+    .component('pv-password', Password)
+    .component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-toast', Toast)
+    .use(i18n)
+    .use(router)
+    .use(pinia)
+    .mount('#app')
